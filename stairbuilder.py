753bdf633f699e067d4760d754c85e3f391362c2
Lawrence D'Oliveiro <ldo@geek-central.gen.nz>
2017-07-07 06:48:18 +0000

    use relative imports

--- stairbuilder.py
+++ stairbuilder.py
@@ -69,12 +69,12 @@
 #-----------------------------------------------------------
 # BEGIN NEW B2.5/Py3.2 CODE
 import bpy
-from add_mesh_building_objects.general import General
-from add_mesh_building_objects.post import Posts
-from add_mesh_building_objects.rail import Rails
-from add_mesh_building_objects.retainer import Retainers
-from add_mesh_building_objects.stringer import Stringer
-from add_mesh_building_objects.tread import Treads
+from .general import General
+from .post import Posts
+from .rail import Rails
+from .retainer import Retainers
+from .stringer import Stringer
+from .tread import Treads
 from bpy.props import (BoolProperty,
                        EnumProperty,
                        IntProperty,
